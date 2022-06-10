d4f001335e86533c04512b2e4ac3854f87a4cab8
DAMI <damikang@DAMIs-MacBook-Pro.local>
2022-06-10 15:15:49 +0100

    styling done.

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,3 +67,41 @@ $(function() {
 	contactForm();
 
 });
+
+
+let hearMap = function() {
+	var platform = new H.service.Platform({
+		 apikey: window.apikey
+	  });   
+	  var defaultLayers = platform.createDefaultLayers();
+	  // Instantiate the map using the vecor map with the
+	  // default style as the base layer:
+	  var map = new H.Map(document.getElementById('map'),
+				 defaultLayers.vector.normal.map,
+				  {  
+				    center: { lat: 55.8685891, lng: -4.2906039 }, //55.868607140200304, -4.289906527549472
+					zoom: 15,  
+					pixelRatio: window.devicePixelRatio || 1
+				  });
+				  window.addEventListener('resize', () => map.getViewPort().resize());
+				  const behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
+				  const request = {
+						// mode: 'fastest;car',
+						// waypoint0: 'geo!37.80221,-122.4191',
+						// waypoint1: 'geo!37.76839,-122.51089',
+						representation: 'display'
+					};
+				  var ui = H.ui.UI.createDefault(map, defaultLayers);
+				  var bubble = new H.ui.InfoBubble(
+					   {
+						lat: 55.8685891, lng: -4.2906039
+					   }, 
+					  {
+						content: `
+							<p> Kelvingrove <br/>
+							Art Gallery and Museum </p>
+							`
+					   });
+				  ui.addBubble(bubble);
+  }
+hearMap();
